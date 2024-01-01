c714e7228da1cad35ec45c723f74788540b98f42
Tamon Okamoto <tamonmon0417@gmail.com>
2024-01-01 19:02:21 +0900

    init and add first server action

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,58 @@
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action
+    'use server'
+
+    const title = formData.get('title') as string
+    const code = formData.get('code') as string
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    })
+
+    console.log(snippet)
+
+    // redirect(`/snippets/${snippet.id}`)
+    redirect('/')
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            id="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+
+        <button type="submit" className="border rounded p-2 bg-blue-200">
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
