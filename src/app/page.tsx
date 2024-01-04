e0421071e765222bdb7fce6c2824fc0280451a36
Tamon Okamoto <tamonmon0417@gmail.com>
2024-01-04 20:19:25 +0900

    show snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Home Page</div>
+import { db } from '@/db'
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
+  const renderSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h2>{snippet.title}</h2>
+      </div>
+    )
+  })
+
+  return <div>{renderSnippets}</div>
 }
