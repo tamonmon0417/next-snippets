7ea45dcd39390972f24cdadc74e053878d773ab6
Tamon Okamoto <tamonmon0417@gmail.com>
2024-01-04 21:56:15 +0900

    show monaco editor

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Editor } from '@monaco-editor/react'
 import type { Snippet } from '@prisma/client'
 
 interface SnippetEditFormProps {
@@ -7,5 +8,15 @@ interface SnippetEditFormProps {
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  return <div>Client component has snippet with title {snippet.title}</div>
+  return (
+    <div>
+      Client component has snippet with title {snippet.title}
+      <Editor
+        height={'40vh'}
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+      />
+    </div>
+  )
 }
