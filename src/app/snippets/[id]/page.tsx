04b61b291dde2b26ad42046127b94155687364be
Tamon Okamoto <tamonmon0417@gmail.com>
2024-01-06 14:19:39 +0900

    generate indivisual page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -44,3 +44,13 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </div>
   )
 }
+
+// SSG
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany()
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    }
+  })
+}
